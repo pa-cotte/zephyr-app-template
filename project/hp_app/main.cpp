9909df89f50aeaed04fb66293f6191094130fee2
jeremie.patron <jeremie.patron@pa.cotte.fr>
2025-06-12 10:04:42 +0200

    Add Support of clickboard 7 wifi with winc1500 driver

--- project/hp_app/main.cpp
+++ project/hp_app/main.cpp
@@ -1,43 +1,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <stdint.h>
 
 #define RED_LED_NODE DT_ALIAS(led0)
+#define SPI3_NODE DT_NODELABEL(spi3)
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(RED_LED_NODE, gpios);
+LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
 
-LOG_MODULE_REGISTER(app);
 
 auto main() -> int
 {
-  int ret;
-
-  k_msleep(1000);
-
-  
-  if (!gpio_is_ready_dt(&led))
-  {
-    LOG_ERR("Led not ready\n");
-    return 0;
-  }
-
-  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-  if (ret < 0)
-  {
-    LOG_ERR("Led config failed\n");
-    return 0;
-  }
-
-  while (true)
-  {
-    LOG_INF("HP red Blink !");
-    ret = gpio_pin_toggle_dt(&led);
-    if (ret < 0)
-    {
-      LOG_ERR("Led toggle failed");
-      return 0;
-    }
-
-    k_msleep(1000);
-  }
+  k_thread_suspend(k_current_get());
 }
