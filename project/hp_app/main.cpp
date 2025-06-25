11855b531dbe3bdade52f2c671e00572f770fef6
jeremie.patron <jeremie.patron@pa.cotte.fr>
2025-06-25 09:11:55 +0200

    Add support of WiFi ESP Click

--- project/hp_app/main.cpp
+++ project/hp_app/main.cpp
@@ -1,16 +1,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/spi.h>
-#include <zephyr/sys/util.h>
-#include <zephyr/sys/printk.h>
-#include <stdint.h>
-
-#define RED_LED_NODE DT_ALIAS(led0)
-#define SPI3_NODE DT_NODELABEL(spi3)
 
 LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
-
 
 auto main() -> int
 {
