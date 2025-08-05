cafbc1b177a27ae2cdc15f41d5d38f0040900f88
jeremie.patron <jeremie.patron@pa.cotte.fr>
2025-08-05 17:27:48 +0200

    Update sysbuild.cmake

--- project/he_app/sysbuild.cmake
+++ project/he_app/sysbuild.cmake
@@ -1,3 +1,3 @@
 set(HP_APP hp_app)
-set(HP_APP_SRC_DIR ./../project/${HP_APP})
-externalzephyrproject_add(APPLICATION ${HP_APP} SOURCE_DIR ${HP_APP_SRC_DIR} BOARD alif_e7_dk/ae722f80f55d5xx/rtss_hp)
+set(HP_APP_SRC_DIR ./../../project/hp_app)
+ExternalZephyrProject_Add(APPLICATION ${HP_APP} SOURCE_DIR ${HP_APP_SRC_DIR} BOARD alif_e7_dk/ae722f80f55d5xx/rtss_hp)
