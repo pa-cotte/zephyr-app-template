5a7ce1d498a29aa744a8534874443d9d6c2647dc
Pauline Briane <pauline.briane@pa.cotte.fr>
2025-10-06 17:22:07 +0200

    test i2c devices on i3c

--- project/he_app/main.cpp
+++ project/he_app/main.cpp
@@ -5,6 +5,8 @@
 #define GREEN_LED_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(GREEN_LED_NODE, gpios);
+static const struct device *i3c = DEVICE_DT_GET(DT_NODELABEL(i3c0));
+static const struct device *pca = DEVICE_DT_GET(DT_NODELABEL(pca9685));
 
 LOG_MODULE_REGISTER(app);
 
@@ -18,6 +20,22 @@ auto main() -> int
     return 0;
   }
 
+  if(!device_is_ready(i3c))
+  {
+    LOG_ERR("I3C device not ready\n");
+    return 0;
+  }else{
+    LOG_INF("I3C device is ready\n");
+  }
+
+  if(!device_is_ready(pca))
+  {
+    LOG_ERR("PCA9685 device not ready\n");
+    return 0;
+  }else{
+    LOG_INF("PCA9685 device is ready\n");
+  }
+
   ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
   if (ret < 0)
   {
@@ -25,16 +43,16 @@ auto main() -> int
     return 0;
   }
 
-  while (true)
-  {
-    LOG_INF("HE green Blink !");
-    ret = gpio_pin_toggle_dt(&led);
-    if (ret < 0)
-    {
-      LOG_ERR("Led toggle failed");
-      return 0;
-    }
+  // while (true)
+  // {
+  //   LOG_INF("HE green Blink !");
+  //   ret = gpio_pin_toggle_dt(&led);
+  //   if (ret < 0)
+  //   {
+  //     LOG_ERR("Led toggle failed");
+  //     return 0;
+  //   }
 
-    k_msleep(500);
-  }
+  //   k_msleep(500);
+  // }
 }
